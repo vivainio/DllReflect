9b29b8acf23383ee961a438aa69cc28c3c540a11
Ville M. Vainio <ville.vainio@basware.com>
2018-08-15 12:21:06 +0300

    works

--- DllReflect/Program.fs
+++ DllReflect/Program.fs
@@ -1,7 +1,45 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System.IO
+open System.Reflection
+open System.Diagnostics
+
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+type Mode = 
+    | Debug
+    | Release
+    | Bad
+
+let parseDebugFlags (dflags: DebuggableAttribute.DebuggingModes) =
+    let hasdefault = 
+        (dflags &&& DebuggableAttribute.DebuggingModes.Default) = DebuggableAttribute.DebuggingModes.Default
+    if hasdefault then Debug else Release
+
+let readAsm f = 
+    try 
+
+        let a = Assembly.LoadFrom f
+        let flags = a.GetCustomAttribute<DebuggableAttribute>().DebuggingFlags
+        
+        (parseDebugFlags flags, f)
+    with
+    | ex -> (Bad, f)
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    0 // return an integer exit code
+    let d = @"."
+    let files = Directory.GetFiles(d, "*.dll", SearchOption.AllDirectories)
+    let all = 
+        files
+        |> Seq.map readAsm
+        |> Seq.groupBy fst
+
+    all
+        |> Seq.iter (
+            fun (g, files) ->
+                printfn "%A" g
+                files |> Seq.map snd |> Seq.iter (printfn "  %s"))
+    match Seq.exists (fun (p,_) -> p = Debug) all with
+    | true -> 2
+    | false -> 0
+           
